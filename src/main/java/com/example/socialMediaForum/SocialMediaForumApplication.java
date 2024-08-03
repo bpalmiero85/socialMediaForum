460899400237715e77bcefb62793ff58be2bc18b
bpalmiero85 <bpalmiero85@gmail.com>
2024-08-02 22:10:21 -0400

    Setup backend

---
+++ src/main/java/com/example/socialMediaForum/SocialMediaForumApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialMediaForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/socialMediaForum/SocialMediaForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialMediaForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
